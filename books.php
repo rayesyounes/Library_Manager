33480e81ddbd01e1ecd887e67c7661333981e7db
rayesyounes <rayesyounes88@gmail.com>
2023-06-08 15:16:27 +0100

    update

--- books.php
+++ books.php
@@ -14,14 +14,18 @@
     <div id="wrapper">
 
         <?php include("sidebar.php"); ?>
-        
+
         <div class="d-flex flex-column" id="content-wrapper">
             <div id="content">
 
                 <?php include("navbar.php"); ?>
 
                 <div class="container-fluid">
-                    <h3 class="text-dark mb-4">Books</h3>
+                    <div class="d-sm-flex justify-content-between align-items-center mb-2">
+                        <h3 class="text-dark mb-4">Books</h3>
+                        <a class="btn btn-primary btn-sm d-none d-sm-inline-block" role="button" href="#">&nbsp;Add
+                            Book</a>
+                    </div>
                     <div class="card shadow">
                         <div class="card-header py-3">
                             <p class="text-primary m-0 fw-bold">Book Info</p>
@@ -36,7 +40,8 @@
                                                 <option value="25">25</option>
                                                 <option value="50">50</option>
                                                 <option value="100">100</option>
-                                            </select>&nbsp;</label></div>
+                                            </select>&nbsp;</label>
+                                    </div>
                                 </div>
                                 <div class="col-md-6">
                                     <div class="text-md-end dataTables_filter" id="dataTable_filter"><label
